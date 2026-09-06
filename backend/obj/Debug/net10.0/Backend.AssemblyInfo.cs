a0d5aa31f70027213a705496e1dbc686562855ab
Tashrik Zim <tanjiltashrikzim@gmail.com>
2026-09-06 16:16:43 +0600

    add docker deployment config

--- backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8a6d2c8367934fc68ffd55f62036ed5904c1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e82448da35a28546abb55e7ccac5f8a5b7ab41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
